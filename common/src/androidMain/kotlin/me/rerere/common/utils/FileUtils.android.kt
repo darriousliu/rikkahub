9f6d6542fc63857f58b6b047b5235639e2b1f477
Darrious Liu <60057825+darriousliu@users.noreply.github.com>
2026-02-10 21:24:04 +0800

    feat: 增强文件删除功能，支持多种内容提供者
    - 实现了对 DocumentsProvider 和 MediaStore 的支持，允许通过 ContentResolver 删除文件。
    - 添加了错误处理，确保在权限不足或其他异常情况下记录错误信息。

--- common/src/androidMain/kotlin/me/rerere/common/utils/FileUtils.android.kt
+++ common/src/androidMain/kotlin/me/rerere/common/utils/FileUtils.android.kt
@@ -2,9 +2,11 @@
 
 package me.rerere.common.utils
 
+import android.content.Context
+import android.provider.DocumentsContract
 import androidx.core.content.FileProvider
 import androidx.core.net.toFile
-import androidx.core.net.toUri
+import co.touchlab.kermit.Logger
 import coil3.Uri
 import coil3.toAndroidUri
 import coil3.toCoilUri
@@ -16,6 +18,7 @@ import java.io.File
 import kotlin.io.deleteRecursively
 import kotlin.io.readBytes
 import kotlin.use
+import org.koin.mp.KoinPlatform
 
 private const val TAG = "FileUtils"
 
@@ -45,7 +48,28 @@ actual fun getUriForFile(
 actual fun PlatformFile.deleteRecursively(): Boolean {
     return if (isContentUri()) {
         val uri = (androidFile as AndroidFile.UriWrapper).uri
-        // todo
+        try {
+            val context = KoinPlatform.getKoin().get<Context>() as Context
+            val resolver = context.contentResolver
+            // 方案 1: 针对 DocumentsProvider (SAF 选择的文件通常是这种)
+            // 检查是否支持 DocumentsContract
+            if (DocumentsContract.isDocumentUri(context, uri)) {
+                return DocumentsContract.deleteDocument(resolver, uri).also {
+                    if (it) Logger.d(TAG) { "DocumentsContract 删除成功: $uri" }
+                }
+            }
+            // 方案 2: 针对 MediaStore (如相册/音频) 或其他 ContentProvider
+            // ContentResolver.delete 返回受影响的行数，>0 表示有动作
+            val rowsDeleted = resolver.delete(uri, null, null)
+            if (rowsDeleted > 0) {
+                Logger.d(TAG) { "ContentResolver 删除成功: $uri" }
+                return true
+            }
+        } catch (e: SecurityException) {
+            Logger.e(TAG, e) { "删除失败: 没有权限 (请检查是否使用了 ACTION_OPEN_DOCUMENT)" }
+        } catch (e: Exception) {
+            Logger.e(TAG, e) { "删除失败: 未知错误" }
+        }
         true
     } else {
         toFile().deleteRecursively()
@@ -54,7 +78,11 @@ actual fun PlatformFile.deleteRecursively(): Boolean {
 
 
 actual fun PlatformFile.toUri(context: PlatformContext): Uri {
-    return toFile().toUri().toCoilUri()
+    return if (isContentUri()) {
+        (androidFile as AndroidFile.UriWrapper).uri.toCoilUri()
+    } else {
+        getUriForFile(context, this)
+    }
 }
 
 /**
