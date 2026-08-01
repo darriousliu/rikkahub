7872d8bb43bbd91da7d41def7fa68359bbfc09d9
Darrious Liu <60057825+master-lzh@users.noreply.github.com>
2026-08-01 19:35:52 +0800

    refactor(material3): 迁移动态色到 commonMain
    - 将 DynamicSchemeExt 从 androidMain 迁至 commonMain
    - 调整模块 source set 与 material-color-utilities 引用

--- material3/build.gradle.kts
+++ material3/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kmp.library)
+    alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.kotlin.compose)
 }
 
@@ -27,9 +28,15 @@ kotlin {
     iosSimulatorArm64()
 
     sourceSets {
-        androidMain.dependencies {
-            implementation(project.dependencies.platform(libs.androidx.compose.bom))
-            implementation(libs.androidx.material3)
+        commonMain {
+            kotlin.srcDir("material-color-utilities/kotlin")
+            dependencies {
+                implementation(compose.material3)
+                implementation(compose.ui)
+            }
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
         named("androidHostTest") {
             dependencies {
@@ -42,11 +49,5 @@ kotlin {
                 implementation(libs.androidx.espresso.core)
             }
         }
-    }
-}
-
-androidComponents {
-    onVariants {
-        it.sources.kotlin?.addStaticSourceDirectory("material-color-utilities/kotlin")
     }
 }
